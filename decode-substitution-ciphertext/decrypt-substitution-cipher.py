9b15a3e97093ba394cef38844b0a935e6f92c66e
Austin Hunt <huntaj@g.cofc.edu>
2022-09-17 13:56:32 -0400

    add parts 1 and 2 work

---
+++ decode-substitution-ciphertext/decrypt-substitution-cipher.py
@@ -0,0 +1,165 @@
+"""
+Script for decrypting the contents of enc.txt encrypted with a substitution cipher.
+
+The contents of enc.txt is a famous text that has been encoded using a substitution cipher.
+As we've learned in class, a frequency analysis can be done of these types of texts to ascertain the key
+- or something very near the key.
+
+Tutorial on using / cracking substitution ciphers: https://www.youtube.com/watch?v=LhS8N6oJdno
+(both caesar shifts and random substitutions)
+
+"""
+from string import ascii_lowercase
+import json
+
+
+def decrypt_content_with_key(key):
+    """ given a key (a map between characters of the alphabet representing
+    substitutions to make) decrypt text encrypted with substitution cipher;
+    replace with capital letter to avoid overwriting a replacement since
+    source is purely lowercase """
+    with open('enc.txt') as f:
+        content = f.read()
+    decrypted = content
+    for enc, dec in key.items():
+        # to avoid overwriting on future iteration if dec is a key in the key dict.
+        decrypted = decrypted.replace(enc, dec.upper())
+    with open('dec.txt', 'w') as f:
+        f.write(decrypted)
+
+
+def get_character_frequencies():
+    """ Return a dictionary of the counts of each character in the encrypted content """
+    with open('enc.txt') as f:
+        content = f.read()
+    freqs = {}
+    for char in content:
+        if char in freqs:
+            freqs[char] += 1
+        else:
+            freqs[char] = 1
+    # sort by value/counts (descending)
+    return {k: v for k, v in sorted(freqs.items(), key=lambda item: item[1], reverse=True)}
+
+
+def get_word_freqencies():
+    with open('enc.txt') as f:
+        content = f.read()
+    _split = content.split()
+    for i in range(1, 7):
+        words_of_this_length = [el for el in _split if len(el) == i]
+        # now get frequencies of words of this length
+        freqs = {}
+        for w in words_of_this_length:
+            if w in freqs:
+                freqs[w] += 1
+            else:
+                freqs[w] = 1
+        # sort by value/counts (descending)
+        freqs = {k: v for k, v in sorted(
+            freqs.items(), key=lambda item: item[1], reverse=True)}
+        with open(f'words-of-length-{i}.json', 'w') as f:
+            json.dump(freqs, f)
+
+
+get_word_freqencies()
+
+# What I know at this point:
+# q is most common single-letter word. h is next most common single letter word.
+# replace q with a and replace h with i.
+
+key = {
+    'q': 'a',
+    'h': 'i',
+}
+# also know that qbd is second most common 3 letter word behind esz. qbd is likely "and",
+key['b'] = 'n'
+key['d'] = 'd'
+
+# esz is very likely "the"
+key['e'] = 't'
+key['s'] = 'h'
+key['z'] = 'e'
+
+# extrapolating from there; common words beginning with the with other letters include:
+# then
+# them
+# they
+# their
+
+# eszpz is second most common 5 letter word.
+# eszhp is 4th most common.
+# z repeats in eszpz. Definitely "there". So second one must be "their":
+key['p'] = 'r'
+key['h'] = 'i'
+
+# eshw is very common; given above, that's "this"
+key['w'] = 's'
+
+# also know esqe is most common 4 letter word. given above, that's "that". makes sense.
+# also, ches is second most common 4 letter word.
+# key['c'] = ''
+
+# ai is most common 2 letter word. considered "is" but s already used, so try "it".
+key['a'] = 'i'
+key['i'] = 't'
+
+
+decrypt_content_with_key(key)
+
+# found phrase kHARfES DIkoENS after the above.
+key['k'] = 'c'
+key['o'] = 'k'
+key['f'] = 'l'
+
+decrypt_content_with_key(key)
+
+# now have: THE nRIyECT tjTENgERt EgIIK IT A TALE IT TcI CITIES, gr CHARLES DICKENS
+# replace keys a and i ; don't want IT, want OF
+key['a'] = 'o'
+key['i'] = 'f'
+# also, c must be w
+key['c'] = 'w'
+
+# gr should be "by" - "by Charles Dickens"
+key['g'] = 'b'
+key['r'] = 'y'
+
+decrypt_content_with_key(key)
+
+# THE nROyECT tjTENBERt EBOOK OF A TALE OF TWO CITIES, BY CHARLES DICKENS
+# should be
+# THE PROJECT GUTENBERG EBOOK OF A TALE OF TWO CITIES, BY CHARLES DICKENS
+key['n'] = 'p'
+key['y'] = 'j'
+key['t'] = 'g'
+key['j'] = 'u'
+
+decrypt_content_with_key(key)
+
+# remaining is a bit obvious.
+key['v'] = 'v'
+key['u'] = 'm'
+
+decrypt_content_with_key(key)
+
+
+def get_remaining_characters_not_in_key(key):
+    # identify remaining characters to finish map
+    return [l for l in ascii_lowercase if l not in key.keys()]
+
+
+remaining = get_remaining_characters_not_in_key(key)
+print(remaining)
+# l, m, x
+key['m'] = 'q'  # mUEEN repeated
+key['x'] = 'x'  # SIx, VExED, BOx, used in roman numerals
+key['l'] = 'z'  # THAT’S A BLAlING STRANGE ANSWER, TOO; DOlEN
+
+assert not get_remaining_characters_not_in_key(key)
+
+sorted_key = dict(sorted(key.items()))
+decrypt_content_with_key(sorted_key)
+with open('key.txt', 'w') as f:
+    for enc, dec in sorted_key.items():
+        f.write(f'{enc}: {dec}\n')
---
+++ find-hash-collision/main.py
@@ -0,0 +1,114 @@
+"""Austin Hunt
+14 Sept 2022
+
+Find two messages (strings or numbers) whose hexdigest collides in the first five characters.
+
+Example provided:
+sha256(bytes(1000).hexdigest())[:4] == sha256(bytes(344962).hexdigest())[:4]
+"""
+from hashlib import sha256
+from concurrent.futures import ThreadPoolExecutor, wait
+import time
+import json
+import os
+from string import ascii_letters
+import random
+
+PYTHONHASHSEED = 175
+
+
+class CollisionFinder:
+    def __init__(self):
+        self.hashes = {}
+
+    def calculate_number_hashes(self, start, end_inclusive):
+        print(
+            f'Starting hash calculations for range {start} to {end_inclusive}')
+        for i in range(start, end_inclusive + 1):
+            hash_first_five = sha256(bytes(i)).hexdigest()[:5]
+            if hash_first_five in self.hashes:
+                self.hashes[hash_first_five].append(i)
+            # think there's an issue here producing inconsistent results with multithreading.
+            # if thread 1 gets to else; thread 2 takes over and sets self.hashes[hash_first_five] to [12345] then thread 1 overwrites.
+            # non-deterministic results. e.g. 95 collisions total for execution 1, 105 for another, 88 for another.
+            # not too important for this assignment but worth investigating.
+            else:
+                self.hashes[hash_first_five] = [i]
+
+    def work_numbers(self):
+        futures = []
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            for i in range(0, 100000, 1000):
+                if i == 0:
+                    start = 0
+                else:
+                    start = i + 1
+                end = i + 100
+                futures.append(
+                    executor.submit(self.calculate_number_hashes, start, end)
+                )
+        for f in wait(futures).done:
+            pass
+
+    def generate_random_string_of_length_n(self, n):
+        return ''.join(random.choices(ascii_letters, k=n))
+
+    def calculate_string_hashes(self, start, end_inclusive):
+        NUM_RANDOM_STRINGS = 5
+        print(
+            f'Starting string hash calculations for random strings between lengths {start} and {end_inclusive}')
+        for i in range(start, end_inclusive + 1):
+            for j in range(NUM_RANDOM_STRINGS):
+                rand_string = self.generate_random_string_of_length_n(i)
+                hash_first_five = sha256(
+                    bytes(rand_string.encode('utf-8'))).hexdigest()[:5]
+                if hash_first_five in self.hashes:
+                    self.hashes[hash_first_five].append(rand_string)
+                else:
+                    self.hashes[hash_first_five] = [rand_string]
+
+    def work_strings(self):
+        futures = []
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            for rand_message_length in range(1, 1000, 25):
+                start = rand_message_length + 1
+                end = rand_message_length + 25
+                futures.append(
+                    executor.submit(self.calculate_string_hashes, start, end)
+                )
+            for f in wait(futures).done:
+                pass
+
+    def save_hashes(self):
+        print('Saving hashes')
+        with open('hashes.json', 'w') as f:
+            json.dump(self.hashes, f)
+
+    def get_partial_collisions(self):
+        return [(hash_collided, src) for hash_collided, src in self.hashes.items() if len(src) > 1]
+
+
+if __name__ == "__main__":
+    finder = CollisionFinder()
+    start = time.time()
+    finder.work_numbers()
+    end = time.time()
+    print(f'Elapsed time for numeric hash calculations: {end - start} seconds')
+    start = time.time()
+    finder.work_strings()
+    end = time.time()
+    print(f'Elapsed time for string hash calculations: {end - start} seconds')
+
+    print('Saving hashes')
+    finder.save_hashes()
+
+    print('Collecting all partial collisions from saved hashes')
+    partial_collisions = finder.get_partial_collisions()
+    print(f'Found {len(partial_collisions)} partial collisions')
+    fpath = os.path.join(os.path.dirname(__file__), 'partial-collisions.txt')
+    with open(fpath, 'w') as f:
+        for pc in partial_collisions:
+            f.write(
+                f'These {len(pc[1])} numbers/strings when hashed produce a partial collision '
+                f'(shared first 5 hash value characters of '
+                f'{pc[0]}): {", ".join([str(el) for el in pc[1]])} \n')
